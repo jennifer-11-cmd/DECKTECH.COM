164ae2b3dad1d17544470acc329f04d844348e18
Jennifer <jennifereneh05@gmail.com>
2024-07-28 15:21:44 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("menu");
+
+function dropDown() {
+  if (menu.style.display == "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}
